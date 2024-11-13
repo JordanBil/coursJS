8a1f0a93ac0a527607b6c3710bd2c23f57eee3cb
jordan <jordan-billon@hotmail.fr>
2024-11-13 15:11:22 +0100

    ajout exercice scroll

--- script.js
+++ script.js
@@ -9,13 +9,10 @@ let monToggle = false;
     monToggle = !monToggle;
  })
 
-let mesLiens = document.querySelectorAll(".superLien");
+
 let ajouter = document.querySelector(".ajouter");
 let supprimer = document.querySelector(".supprimer");
 let toggle = document.querySelector(".toggle");
-let ternaire = false ;
-
-
 
 ajouter.addEventListener("click" ,()=>{
     titre.classList.add('monStyle');
@@ -27,5 +24,39 @@ supprimer.addEventListener("click" ,()=>{
 
 toggle.addEventListener("click" ,()=>{
     titre.classList.toggle('monStyle');
-} )
+} );
 
+
+
+document.addEventListener("click" ,function(event){
+    let x = event.clientX;
+    let y = event.clientY;
+    let image = document.createElement('img');
+    let tailleIMG = 100;
+    image.src = "./coeur.png";
+    image.style.position = "absolute";
+    image.style.width = tailleIMG + 'px';
+    image.style.left = event.x - tailleIMG /2 + 'px' ;
+    image.style.top = event.y - tailleIMG /2 + 'px' ;
+    document.body.appendChild(image);
+}) ;
+
+VANTA.FOG({
+    el: "#app",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00
+})
+
+  laBar = document.querySelector(".bar");
+
+    document.addEventListener("scroll" , ()=>{
+    console.log(document.body.scrollHeight, window.innerHeight, window.scrollY);
+    scrollMax = document.body.scrollHeight - window.innerHeight;
+    onEstOu = window.scrollY/scrollMax;
+    laBar.style.width = onEstOu*100 + "%" ; 
+  })
+
+  
