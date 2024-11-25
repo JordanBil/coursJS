e27bcd46d4bf6de20045c3df6917375570da468a
jordan <jordan-billon@hotmail.fr>
2024-11-25 10:15:56 +0100

    exo class

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
+class imc {
 
-VANTA.FOG({
-    el: "#app",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00
-})
+    nom;
+    poids;
+    taille;
+
+    constructor(nom, poids, taille) {
+        this.nom = nom;
+        this.poids = poids;
+        this.taille = taille;
+    }
+
+    // Méthode pour obtenir le nom de l'animal
+    getNom() {
+        return this.nom;
+    }
+
+    // Méthode pour obtenir l'âge de l'animal
+    getPoids() {
+        return this.poids;
+    }
+
+    getTaille() {
+        return this.taille;
+    }
+
+    // Méthode pour afficher les informations de l'animal
+    displayImc() {
+        let imc = this.poids / (this.taille * this.taille);
+        console.log(`l'IMC de ${this.nom} (${this.poids} kg, ${this.taille} m ) est de ${imc.toFixed(2)}`);       
+    }
+}
+
+let list = [
+    new imc("Sébastien Chabal", 135, 1.7),
+    new imc("Escaladeuse", 45, 1.68),
+    new imc("JOJO", 300, 2),
+    new imc("gontrand", 90, 1.75),
+    new imc("Colonel Clock", 200, 1.75),
+    new imc("Josiane de la Véga", 99, 1.55),
+];
+
+list.forEach((person) => {
+    person.displayImc();
+});
+
+
+
